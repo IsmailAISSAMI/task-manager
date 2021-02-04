15fd943b5af5b07db1d419d3bead2a273c7c0a94
ismailaissami <aissami.ismail@gmail.com>
2021-02-04 23:08:31 +0100

    :sparkles: add async/await and update user/task

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 require("./db/mongoose")
 const User = require("./models/user")
 const Task = require("./models/task")
+const { ObjectId } = require('mongodb')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -9,68 +10,109 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 
 // Create users
-app.post('/users', (req, res)=>{
+app.post('/users', async (req, res)=>{
     const user = new User(req.body)    
-    user.save().then((user)=>{
+    try {
+        await user.save()
         res.send(user)
-    }).catch((error)=>{
+    } catch(error){
         res.status(400).send(error)
-    })
+    }
 })
 
 // Read users 
-app.get('/users', (req,res) => {
-    User.find({}).then((users)=>{
+app.get('/users', async (req,res) => {
+    try{
+        const users = await User.find({})
         res.send(users)
-    }).catch((error)=>{
+    } catch(error){
         // Status 500 : Internal Server Error
         res.status(500).send()
-    })
+    }
 })
 
 // Read a single user data 
-app.get('/users/:id', (req, res)=>{
+app.get('/users/:id', async (req, res)=>{
     const _id = req.params.id
-    User.findById(_id).then((user)=>{
+    try{
+        const user = await User.findById(_id)
         if(!user){
             // Status 404: Not Found
-            return res.status(404).send()
+            res.status(404).send()
+        }
+    } catch(error){
+        res.status(500).send(error)
+    }
+})
+
+app.patch('/users/:id', async (req,res)=>{
+    // if the user want to update a field that doesn't exist 
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ["name", "email", "password"]
+    const isValidOperation = updates.every((update)=> allowedUpdates.includes(update))
+    if(!isValidOperation){
+        res.status(400).send({'error': 'invalid update!'})
+    }
+    try{
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+        if(!user){
+            res.satatus(404).send()
         }
         res.send(user)
-    }).catch((error)=>{
-        res.status(500).send()
-    })
+    } catch(error){
+        res.satatus(400).send()
+    }
 })
-
 
 // Part 2 : Tasks
-app.post('/tasks',(req,res)=>{
+app.post('/tasks', async (req,res)=>{
     const task = new Task(req.body)
-    task.save().then((task)=>{
+    try{
+        await task.save()
         res.send(task)
-    }).catch((error)=>{
+    } catch(error){
         res.status(400).send(error)
-    })
+    }
 })
 
-app.get('/tasks', (req,res)=>{
-    Task.find({}).then((tasks)=>{
+app.get('/tasks', async (req,res)=>{
+    try{
+        const tasks = await Task.find({})
         res.send(tasks)
-    }).catch((error)=>{
+    } catch(error){
         res.status(500).send()
-    })
+    }
 })
 
-app.get('/tasks/:id', (req,res)=>{
+app.get('/tasks/:id', async (req,res)=>{
     const _id = req.params.id
-    Task.findById(_id).then((task)=>{
+    try{
+        const task = await Task.findById(_id)
         if(!task){
             return res.status(404).send()
         }
         res.send(task)
-    }).catch((error)=>{
+    } catch(error){
         res.status(500).send()
-    })
+    }
+})
+
+app.patch('/tasks/:id', async (req, res)=>{
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ["description", "completed"]
+    const isValidOperation = updates.every((update)=> allowedUpdates.includes(update))
+    if(!isValidOperation){
+        res.status(400).send({error: "Invalide update!"})
+    }
+    try{
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true , runValidators: true })
+        if(!task){
+            res.status(404).send()
+        }
+        res.send(task)
+    } catch(error){
+        res.status(500).send()
+    }
 })
 
 app.listen(port, ()=>{
